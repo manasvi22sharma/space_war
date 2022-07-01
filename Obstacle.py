195e226fb8c58f1bd5c9ff1b506d846c1e488bd4
manasvi22sharma <sharmamanasvi2208@gmail.com>
2022-07-02 00:18:12 +0530

    intra collision between obstacles

--- Obstacle.py
+++ Obstacle.py
@@ -32,24 +32,42 @@ class Obstacle(pygame.sprite.Sprite):
             self.image=self.frames[self.animation_index]
             self.image=pygame.transform.rotozoom(self.image,0,0.2)
             self.rect=self.image.get_rect(midbottom = (randint(0,599),-5))
+        self.blast=False
+        self.blast_count=0
+        self.blast_animation_index=0
+        self.blast_2=pygame.image.load('images\space_blast2.png').convert_alpha()
+        self.blast_1=pygame.image.load('images\space_blast1.png').convert_alpha()
+        self.blast_frames=[self.blast_2,self.blast_1]
+        self.image = self.blast_frames[int(self.blast_animation_index)]
+        self.image=pygame.transform.rotozoom(self.image,0,0.2)
 
 
     def animation_state(self):
-        self.animation_index += 0.1
-        if self.animation_index >= len(self.frames): self.animation_index = 0
-        self.image = self.frames[int(self.animation_index)]
-        self.image=pygame.transform.rotozoom(self.image,0,0.2)
+        if self.blast:
+            self.blast_animation_index += 0.1
+            if self.blast_animation_index >= len(self.blast_frames): self.blast_animation_index = 0
+            self.image = self.blast_frames[int(self.blast_animation_index)]
+            self.image=pygame.transform.rotozoom(self.image,0,0.15)
+        else:    
+            self.animation_index += 0.1
+            if self.animation_index >= len(self.frames): self.animation_index = 0
+            self.image = self.frames[int(self.animation_index)]
+            self.image=pygame.transform.rotozoom(self.image,0,0.2)
     def update(self):
         self.animation_state()
-        if self.type=='rock':
-            self.rect.x -=1
-            self.rect.y += 2
-        if self.type=='enemy_ship_type1':
-            self.rect.y += 4
-        if self.type=='enemy_ship_type2':
-            self.rect.y +=5
-        self.rect
-        self.destroy()
+        if self.blast and self.blast_count<=18 :
+            self.blast_count+=1
+        elif self.blast and self.blast_count>18:
+            self.kill()
+        else:    
+            if self.type=='rock':
+                self.rect.x -=1
+                self.rect.y += 2
+            if self.type=='enemy_ship_type1':
+                self.rect.y += 4
+            if self.type=='enemy_ship_type2':
+                self.rect.y +=5
+            self.destroy()
 
     def destroy(self):
         if self.rect.y>= 610:
--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from Obstacle import Obstacle
 from Player import Player
 from Space import Space
 
+def collisons_between_obstacles(obstacles):
+    for obstacle in obstacles.sprites():
+        list_collsions=pygame.sprite.spritecollide(obstacle,obstacles,False)
+        if len(list_collsions)>=2:
+            for sprite in list_collsions:
+                sprite.blast=True
+        
+
+
 pygame.init()
 clock=pygame.time.Clock()
 screen=pygame.display.set_mode((500,600))
@@ -15,14 +24,14 @@ obstacles=pygame.sprite.Group()
 obstacles.add(Obstacle('rock'))
 #timmer
 obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer,1500)
+pygame.time.set_timer(obstacle_timer,1000)
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
         if event.type==obstacle_timer:
-            obstacles.add(Obstacle(choice(['enemy_ship_type1','enemy_ship_type1','enemy_ship_type2','rock'])))
+            obstacles.add(Obstacle(choice(['enemy_ship_type1','enemy_ship_type1','enemy_ship_type1','enemy_ship_type1','enemy_ship_type1','enemy_ship_type2','rock'])))
 
     screen.blit(space.image, (-80, -30))
     space.space_animation()
@@ -30,6 +39,7 @@ while True:
     player.sprite.player_update()
     obstacles.draw(screen)
     obstacles.update()
+    collisons_between_obstacles(obstacles)
 
     pygame.display.update()
     clock.tick(60)
