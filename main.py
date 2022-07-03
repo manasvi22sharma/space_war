4f4f86a8ce10a31283413b3a056a64c10cac64c5
manasvi22sharma <sharmamanasvi2208@gmail.com>
2022-07-03 16:45:25 +0530

    Add bullets and score display

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from secrets import choice
+from tkinter import CENTER
 import pygame
 from sys import exit
 from Bullet import Bullet
@@ -27,7 +28,20 @@ def collsion_player(obstacles,player_group):
             sprite.blast=True
     #print(list_collison)
     return True
-
+def display_bullets_left(bullets_left):
+    if bullets_left>15:
+        bullets_lef_message=test_font.render(f'Bullets: {bullets_left}',False,(93,227,9))
+    else:
+        bullets_lef_message=test_font.render(f'Bullets: {bullets_left}',False,(245,7,43))
+    bullets_lef_message_rect=bullets_lef_message.get_rect(center=(50,50))
+    screen.blit(bullets_lef_message,bullets_lef_message_rect)
+    
+def display_score(start_time):
+	current_time = int(pygame.time.get_ticks() / 1000) - start_time
+	score_surf = test_font.render(f'Score: {current_time}',False,(10,98,245))
+	score_rect = score_surf.get_rect(center = (450,50))
+	screen.blit(score_surf,score_rect)
+	return current_time
 
 pygame.init()
 clock=pygame.time.Clock()
@@ -38,6 +52,9 @@ player_group.add(player)
 space=Space()
 obstacles=pygame.sprite.Group()
 obstacles.add(Obstacle('rock'))
+test_font = pygame.font.Font('font/Pixeltype.ttf', 30)
+bullets_left=100
+start_time=0
 game_active=True
 #timmer
 obstacle_timer = pygame.USEREVENT + 1
@@ -52,7 +69,9 @@ while True:
                 obstacles.add(Obstacle(choice(['enemy_ship_type1','enemy_ship_type1','enemy_ship_type1','enemy_ship_type1','enemy_ship_type1','enemy_ship_type2','rock'])))
             if event.type==pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    fire_bullet(player,player_group)
+                    if bullets_left>0:
+                        fire_bullet(player,player_group)
+                        bullets_left -=1
         
     if game_active:
         screen.blit(space.image, (-80, -30))
@@ -63,6 +82,8 @@ while True:
         obstacles.update()
         collisons_between_obstacles(obstacles)
         game_active=collsion_player(obstacles,player_group)
+        display_bullets_left(bullets_left)
+        display_score(start_time)
 
     pygame.display.update()
     clock.tick(60)
